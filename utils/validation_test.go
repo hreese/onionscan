deac94d4445217e3dcda8e04ab1075c46a5b36f6
Heiko Reese <mail@heiko-reese.de>
2022-06-30 10:29:58 +0200

    Updated address validation to only support Onion V3 addresses.

--- utils/validation_test.go
+++ utils/validation_test.go
@@ -6,29 +6,34 @@ import (
 
 func TestIsOnion(t *testing.T) {
 
-	test := IsOnion("maschqwtr4lqt2pj.onion")
+	// proper onion v3 address
+	test := IsOnion("testq3qwbiwdtitdydemcez56oenag25gdym4xmhraoqbsmrsrqnagad.onion")
 	if !test {
-		t.Errorf("%s should have matched!", "maschqwtr4lqt2pj.onion")
+		t.Errorf("%s should have matched!", "testq3qwbiwdtitdydemcez56oenag25gdym4xmhraoqbsmrsrqnagad.onion")
 	}
 
-	test = IsOnion("subdomain.lots.of.maschqwtr4lqt2pj.onion")
+	// proper onion v3 address with subdomains
+	test = IsOnion("subdomain.lots.of.test3uasslvk65oxtcbrane7bqvuqbv5wuaomu4gmf3wjxi7c5kxgiad.onion")
 	if !test {
-		t.Errorf("%s should have matched!", "subdomain.lots.of.maschqwtr4lqt2pj.onion")
+		t.Errorf("%s should have matched!", "subdomain.lots.of.test3uasslvk65oxtcbrane7bqvuqbv5wuaomu4gmf3wjxi7c5kxgiad.onion")
 	}
 
-	test = IsOnion("thisis9notavalidonion.onion")
+	// address with charset outside of specified encoding
+	test = IsOnion("thisis9notavalidonionbecauseitcontainsinvalidenc1letters.onion")
 	if test {
-		t.Errorf("%s should not have matched!", "thisis9notavalidonion.onion")
+		t.Errorf("%s should not have matched!", "thisis9notavalidonionbecauseitcontainsinvalidenc1letters.onion")
 	}
 
+	// does not end in .onion
 	test = IsOnion("maschqwtr4lqt2pj.com")
 	if test {
 		t.Errorf("%s should not have matched!", "maschqwtr4lqt2pj.com")
 	}
 
-	test = IsOnion("www.thisisnotanonionitistoolong.onion")
+	// address too long
+	test = IsOnion("www.thisisnotanonionitistoolongwwwwwwwwwwwwwwwwwwwwwwwwww.onion")
 	if test {
-		t.Errorf("%s should not have matched!", "www.thisisnotanonionitistoolong.onion")
+		t.Errorf("%s should not have matched!", "www.thisisnotanonionitistoolongwwwwwwwwwwwwwwwwwwwwwwwwww.onion")
 	}
 
 }
